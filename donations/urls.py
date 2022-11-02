6977a9605f77e58ebc818a598149fcb7c27f4f72
Pavel Niakhai <3055963@mail.ru>
2022-11-02 16:55:48 +0300

    fix problem with upload image file

--- donations/urls.py
+++ donations/urls.py
@@ -1,7 +1,6 @@
-from django.conf.urls.static import static
 from django.urls import path, re_path, include
 from . import views
-from django.conf import settings
+
 
 app_name = 'donations'
 
@@ -16,8 +15,7 @@ urlpatterns = [
     re_path('donate/(?P<id>[\d-]+)/comment', views.donate_comment, name='donate_comment'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
 
