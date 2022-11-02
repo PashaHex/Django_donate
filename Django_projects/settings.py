80bcf4703bf41c670ae9085086db3a0cf27483ee
Pavel Niakhai <3055963@mail.ru>
2022-11-02 15:30:38 +0300

    add footer (not finish)

--- Django_projects/settings.py
+++ Django_projects/settings.py
@@ -130,9 +130,6 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = 'media_files'
 
-# STATICFILES_DIRS = [
-#     'description_photo',
-# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
